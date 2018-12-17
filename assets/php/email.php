7f7d5d8fe9c288f85573a9f895c4699f50292ae7
José Quiroz <jo_quiroz@outlook.com>
2018-12-17 14:10:21 -0500

    update

--- assets/php/email.php
+++ assets/php/email.php
@@ -1,8 +1,8 @@
 <?php
 mb_internal_encoding("UTF-8");
 
-$to = 'hello@example.com';
-$subject = 'Message from Cryptex';
+$to = 'info@cubesquad.org';
+$subject = 'Mensaje en la Web';
 
 $name = "";
 $email = "";
